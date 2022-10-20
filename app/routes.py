91863835dbcc1ae9da9a71cb2614b29ff6c8a942
Ariel <gabriellaw15@gmail.com>
2022-10-20 15:04:33 -0700

    Created function to handle reading one book-actually this time

--- app/routes.py
+++ app/routes.py
@@ -1,31 +1,31 @@
 from flask import Blueprint, jsonify
 
-hello_world_bp = Blueprint("hello_world", __name__)
+# hello_world_bp = Blueprint("hello_world", __name__)
 
-@hello_world_bp.route("/hello-world", methods=["GET"])
-def say_hello_world():
-    response_body = "Hello, World!"
-    return response_body
+# @hello_world_bp.route("/hello-world", methods=["GET"])
+# def say_hello_world():
+#     response_body = "Hello, World!"
+#     return response_body
 
-@hello_world_bp.route("/hello/JSON", methods=["GET"])
-def say_hello_json():
-    response_body = {
-        "name": "Ariel Wilson",
-        "message": "Hello!",
-        "hobbies": ["Painting", "Sleeping", "Watching TV"]
-    }
-    return response_body
+# @hello_world_bp.route("/hello/JSON", methods=["GET"])
+# def say_hello_json():
+#     response_body = {
+#         "name": "Ariel Wilson",
+#         "message": "Hello!",
+#         "hobbies": ["Painting", "Sleeping", "Watching TV"]
+#     }
+#     return response_body
 
-@hello_world_bp.route("/broken-endpoint-with-broken-server-code", methods=["GET"])
-def broken_endpoint():
-    response_body = {
-        "name": "Ada Lovelace",
-        "message": "Hello!",
-        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-    }
-    new_hobby = "Surfing"
-    response_body["hobbies"].append(new_hobby)
-    return response_body
+# @hello_world_bp.route("/broken-endpoint-with-broken-server-code", methods=["GET"])
+# def broken_endpoint():
+#     response_body = {
+#         "name": "Ada Lovelace",
+#         "message": "Hello!",
+#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+#     }
+#     new_hobby = "Surfing"
+#     response_body["hobbies"].append(new_hobby)
+#     return response_body
 
 class Book():
     def __init__(self, id, title, description):
@@ -50,3 +50,15 @@ def handle_books():
             "description": book.description
         })
     return jsonify(books_response)
+
+@books_bp.route("/<book_id>", methods=["GET"])
+def handle_book(book_id):
+    book_id = int(book_id)
+    for book in books:
+        if book.id == book_id:
+            return {
+                "id": book.id,
+                "title": book.title,
+                "description": book.description
+            }
+    
