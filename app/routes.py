15d4833f532176fa8883959aca0281572704738f
Ariel <gabriellaw15@gmail.com>
2022-10-19 19:13:10 -0700

    Created second endpoint /hello/JSON

--- app/routes.py
+++ app/routes.py
@@ -6,3 +6,12 @@ hello_world_bp = Blueprint("hello_world", __name__)
 def say_hello_world():
     response_body = "Hello, World!"
     return response_body
+
+@hello_world_bp.route("/hello/JSON", methods=["GET"])
+def say_hello_json():
+    response_body = {
+        "name": "Ariel Wilson",
+        "message": "Hello!",
+        "hobbies": ["Painting", "Sleeping", "Watching TV"]
+    }
+    return response_body
