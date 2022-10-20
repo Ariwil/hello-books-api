4e63f4e3b510328a4451ca7339f0c3fcd67b25dc
Ariel <gabriellaw15@gmail.com>
2022-10-19 19:50:57 -0700

    Added a third endpoint, fixing any initital bugs.

--- app/routes.py
+++ app/routes.py
@@ -15,3 +15,14 @@ def say_hello_json():
         "hobbies": ["Painting", "Sleeping", "Watching TV"]
     }
     return response_body
+
+@hello_world_bp.route("/broken-endpoint-with-broken-server-code", methods=["GET"])
+def broken_endpoint():
+    response_body = {
+        "name": "Ada Lovelace",
+        "message": "Hello!",
+        "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
+    }
+    new_hobby = "Surfing"
+    response_body["hobbies"].append(new_hobby)
+    return response_body
