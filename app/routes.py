3b4cdf1cd06e491192f0f7fa4c06d94138af0310
Ariel <gabriellaw15@gmail.com>
2022-10-26 19:44:50 -0700

    Setup models

--- app/routes.py
+++ app/routes.py
@@ -1,76 +1,49 @@
 from flask import Blueprint, jsonify, abort, make_response
 
-# hello_world_bp = Blueprint("hello_world", __name__)
-
-# @hello_world_bp.route("/hello-world", methods=["GET"])
-# def say_hello_world():
-#     response_body = "Hello, World!"
-#     return response_body
-
-# @hello_world_bp.route("/hello/JSON", methods=["GET"])
-# def say_hello_json():
-#     response_body = {
-#         "name": "Ariel Wilson",
-#         "message": "Hello!",
-#         "hobbies": ["Painting", "Sleeping", "Watching TV"]
-#     }
-#     return response_body
-
-# @hello_world_bp.route("/broken-endpoint-with-broken-server-code", methods=["GET"])
-# def broken_endpoint():
-#     response_body = {
-#         "name": "Ada Lovelace",
-#         "message": "Hello!",
-#         "hobbies": ["Fishing", "Swimming", "Watching Reality Shows"]
-#     }
-#     new_hobby = "Surfing"
-#     response_body["hobbies"].append(new_hobby)
-#     return response_body
-
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description =description
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description =description
         
-books = [
-    Book(1, "Green Eggs and Ham", "Book about green eggs and ham"),
-    Book(2, "Red Fish, Blue Fish", "Book about fishies"),
-    Book(3, "Pride and Prejudice", "Book about love")
-]
+# books = [
+#     Book(1, "Green Eggs and Ham", "Book about green eggs and ham"),
+#     Book(2, "Red Fish, Blue Fish", "Book about fishies"),
+#     Book(3, "Pride and Prejudice", "Book about love")
+# ]
 
 books_bp = Blueprint("books", __name__, url_prefix="/books")
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        abort(make_response({"message": f"book {book_id} invalid"}, 400))
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except:
+#         abort(make_response({"message": f"book {book_id} invalid"}, 400))
 
-    for book in books:
-        if book.id == book_id:
-            return book
+#     for book in books:
+#         if book.id == book_id:
+#             return book
     
-    abort(make_response({"message": f"book {book_id} not found"}, 404))
+#     abort(make_response({"message": f"book {book_id} not found"}, 404))
 
-@books_bp.route("", methods=["GET"])
-def handle_books():
-    books_response = []
-    for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
-    return jsonify(books_response)
+# @books_bp.route("", methods=["GET"])
+# def handle_books():
+#     books_response = []
+#     for book in books:
+#         books_response.append({
+#             "id": book.id,
+#             "title": book.title,
+#             "description": book.description
+#         })
+#     return jsonify(books_response)
 
 
-@books_bp.route("/<book_id>", methods=["GET"])
-def handle_book(book_id):
-    book = validate_book(book_id)
-    print(book)
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description
-    }
+# @books_bp.route("/<book_id>", methods=["GET"])
+# def handle_book(book_id):
+#     book = validate_book(book_id)
+#     print(book)
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description
+#     }
