e2efb0027007b70786a84bd79e55551eaccaa9a5
Ariel <gabriellaw15@gmail.com>
2022-11-06 09:53:06 -0800

    Updated models

--- app/models/book.py
+++ app/models/book.py
@@ -1,13 +1,12 @@
 from app import db
 
 class Book(db.Model):
-    __tablename__ = "book_table"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
     description = db.Column(db.String)
     author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
-    author = db.relationship("Author", back_populates="books")
-    
+    # author = db.relationship("Author", back_populates="books")
+
     def to_dict(self):
         return {
                 "id": self.id,
